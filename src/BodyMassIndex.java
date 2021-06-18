6128323670590eea9398877f981d91fe831a42aa
Leonid Beltsov <leonidbeltsov@gmail.com>
2021-06-18 06:55:09 +0300

    fix 1

--- src/BodyMassIndex.java
+++ src/BodyMassIndex.java
@@ -1,6 +1,6 @@
 public class BodyMassIndex {
     public int calculate(int mass, int height) {
-        int bodyMassIndex = 85*10_000/(height*height);
+        int bodyMassIndex = mass*10_000/(height*height);
         return bodyMassIndex;
     }
 }
